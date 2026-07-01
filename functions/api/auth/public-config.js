86042099d0fbc3c04747d58907428b888814b4b1
KKoma Programmer <ijunu3343@gmail.com>
2026-07-01 20:16:19 +0900

    Remove demo auth config and expose safe site URL

--- functions/api/auth/public-config.js
+++ functions/api/auth/public-config.js
@@ -1,8 +1,11 @@
 import { json } from '../../_lib/response.js';
+
 export async function onRequest({ env }) {
+  const siteUrl = env.PUBLIC_SITE_URL || (env.CF_PAGES_URL ? `https://${env.CF_PAGES_URL}` : '');
   return json({
     supabaseUrl: env.SUPABASE_URL || '',
     supabaseAnonKey: env.SUPABASE_ANON_KEY || '',
-    demoMode: !(env.SUPABASE_URL && env.SUPABASE_ANON_KEY && env.SUPABASE_SERVICE_ROLE_KEY && env.APP_SECRET)
+    authReady: !!(env.SUPABASE_URL && env.SUPABASE_ANON_KEY),
+    siteUrl
   });
 }
